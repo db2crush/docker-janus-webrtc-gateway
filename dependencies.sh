ee201d50d8bf37c11bb829c265b569bec361842d
delbert <db2crush@gmail.com>
2019-09-19 11:34:40 +0900

    fixed dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,4 @@
-apt-get install -y libmicrohttpd-dev libjansson-dev libnice-dev \
-    libssl-dev libsofia-sip-ua-dev libglib2.0-dev \
-    libopus-dev libogg-dev libini-config-dev libcollection-dev \
-    pkg-config gengetopt libtool automake
+apt-get install -y libmicrohttpd-dev libjansson-dev \
+	libssl-dev libsrtp-dev libsofia-sip-ua-dev libglib2.0-dev \
+	libopus-dev libogg-dev libcurl4-openssl-dev liblua5.3-dev \
+	libconfig-dev pkg-config gengetopt libtool automake
